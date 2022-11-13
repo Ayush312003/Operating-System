5a12cbb2d666683168b87f0458b1725c83a60406
Ayush Singh <86509024+Ayush312003@users.noreply.github.com>
2022-11-13 10:50:23 +0530

    Create Wait.c

---
+++ Wait.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<sys/wait.h>
+
+#include<unistd.h>
+
+int main()
+
+{
+
+if(fork()==0)
+
+{
+
+int n;
+
+printf("Child Process ID- %d\n",getpid());
+
+printf("ENter a no. ");
+
+scanf("%d",&n);
+
+if(n&1)
+
+printf("No. is Odd\n");
+
+else
+
+printf("No. is Even\n");
+
+}
+
+else
+
+{
+
+wait(NULL);
+
+int a,b;
+
+printf("Parent Process ID- %d\n",getpid());
+
+printf("ENter 2 no.s ");
+
+scanf("%d %d",&a,&b);
+
+printf("Greater no. is %d",a>b?a:b);
+
+}
+
+return 0;
+
+}
